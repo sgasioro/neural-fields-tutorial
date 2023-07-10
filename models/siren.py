44c6f808d77cf9cc6ea3ad738aa0ed2956806624
Sean Gasiorowski <sgaz@slac.stanford.edu>
2023-07-10 16:18:37 -0700

    Add tutorial notebook + model

---
+++ models/siren.py
@@ -0,0 +1,63 @@
+"""
+Source : https://github.com/vsitzmann/siren
+"""
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+class SineLayer(nn.Module):
+
+    def __init__(self, in_features, out_features, is_first=False, omega_0=30):
+        super().__init__()
+        self.omega_0 = omega_0
+        self.is_first = is_first
+
+        self.in_features = in_features
+        self.linear = nn.Linear(in_features, out_features)
+
+        self.init_weights()
+
+    def init_weights(self):
+        with torch.no_grad():
+            if self.is_first:
+                self.linear.weight.uniform_(-1 / self.in_features,
+                                            1 / self.in_features)
+            else:
+                self.linear.weight.uniform_(-np.sqrt(6 / self.in_features) / self.omega_0,
+                                            np.sqrt(6 / self.in_features) / self.omega_0)
+
+    def forward(self, input):
+        return torch.sin(self.omega_0 * self.linear(input))
+
+
+class Siren(nn.Module):
+    def __init__(self, in_features, hidden_features, hidden_layers, out_features, first_omega_0=30, hidden_omega_0=30.,
+                 outermost_linear=False, outermost_linear_activation=nn.Softplus()):
+        super().__init__()
+
+        self.net = []
+        self.net.append(SineLayer(in_features, hidden_features, is_first=True, omega_0=first_omega_0))
+
+        for i in range(hidden_layers):
+            self.net.append(SineLayer(hidden_features, hidden_features, is_first=False, omega_0=hidden_omega_0))
+
+        if outermost_linear:
+            final_linear = nn.Linear(hidden_features, out_features)
+            with torch.no_grad():
+                final_linear.weight.uniform_(-np.sqrt(6 / hidden_features) / hidden_omega_0,
+                                             np.sqrt(6 / hidden_features) / hidden_omega_0)
+
+            self.net.append(final_linear)
+            self.net.append(outermost_linear_activation)
+
+
+        else:
+            self.net.append(SineLayer(hidden_features, out_features, is_first=False, omega_0=hidden_omega_0))
+
+        self.net = nn.Sequential(*self.net)
+
+    def forward(self, coords):
+        coords = coords.clone().detach().requires_grad_(True)
+        output = self.net(coords)
+        return output, coords
